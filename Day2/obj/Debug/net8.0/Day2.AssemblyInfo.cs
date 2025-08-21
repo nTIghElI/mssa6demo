f08f099be0cf783874d1304c8820305f5966752c
your username <your email>
2025-08-21 17:03:58 -0400

    Final Commit

--- Day2/obj/Debug/net8.0/Day2.AssemblyInfo.cs
+++ Day2/obj/Debug/net8.0/Day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d71d1e1989e85199d1384d37b7aac07222e7a68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b644a0042202f0fa1165c19a39060b981b4ac3b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
